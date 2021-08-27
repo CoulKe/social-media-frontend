f54c2a74cc2c6508e3b7282f32aa54c6f77e3d8f
Luteya Coulston <thatcoul@gmail.com>
2021-08-27 23:39:49 +0300

    Fixed corrupt

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -26,17 +26,12 @@ const profileReducers = (
       return { ...state, loading: true };
     case profileTypes.FETCH_USER_POSTS_SUCCESS:
       return { ...action.payload.data, loading: false };
-<<<<<<< HEAD
     case profileTypes.RESET_UPDATING:
       return { ...state, updated: false, updating: false };
     case profileTypes.UPDATE_USER_INFO_REQUEST:
       return { ...state, updated: false, updating: true };
     case profileTypes.UPDATE_USER_INFO:
       return { ...state, userInfo: action.payload.data, updated: true };
-=======
-    case profileTypes.UPDATE_USER_INFO:
-      return { ...state, userInfo: action.payload.data };
->>>>>>> fc7e827a2753cddf3744fde5cb17aec225f50347
     case TOGGLE_FOLLOW:
       return { ...state, ...action.payload.data };
 
